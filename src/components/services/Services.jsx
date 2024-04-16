bc51971e61bd18f38b9210969fdac00fb49b4b11
lionel3107 <dlmsonelterror@gmail.com>
2024-04-16 12:40:43 +0000

    after debug

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./services.css";
 
 const Services = () => {
+    const [toggleState, setToggleState] = useState(0);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    }
   return (
     <section className='services section' id='services'>
         <h2 className="section__title">Services</h2>
@@ -16,11 +21,14 @@ const Services = () => {
                     </h3>
                 </div>
 
-                <span className="services__button">View More <i className="uil uil-arrow-right services__button-icon"></i></span>
+                <span className="services__button" onClick={() => toggleTab(1)}>
+                    View More 
+                   <i className="uil uil-arrow-right services__button-icon"></i>
+                </span>
 
-                <div className="services__modal">
+                <div className={toggleState === 1 ? "services__modal active-modal" : "services__modal"}>
                     <div className="services__modal-content">
-                        <i className="uil uil-times services__modal-close"></i>
+                        <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
 
                         <h3 className="services__modal-title">Product Designer</h3>
                         <p className="services__modal-description">Service
